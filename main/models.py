1718f6045c8e3a2e8effc013d7d2bf4d813088e5
Banzaika <herovichrick@gmail.com>
2023-12-03 14:59:48 +0300

    reformat project with black, isort

--- main/models.py
+++ main/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Password(models.Model):
     service = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    
+
     class Meta:
-        db_table = 'passwords'
+        db_table = "passwords"
