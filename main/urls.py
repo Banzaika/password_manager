efad94514a42e917f96db305e4094f7d92644cfc
Banzaika <herovichrick@gmail.com>
2023-12-04 13:50:28 +0300

    add routes

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import PasswordCreateRetrieveGetView, PasswordSearchView
 urlpatterns = [
-    path('password/<str:service_name>', PasswordCreateRetrieveGetView.as_view()),
-    path('password/', PasswordSearchView.as_view()),
+    path('password/<str:service_name>', PasswordCreateRetrieveGetView.as_view(), name='password-get-post'),
+    path('password/', PasswordSearchView.as_view(), name='pass-search'),
 ]
